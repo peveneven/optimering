4bf241355768a846c70926e89b5ab1f59c307d90
peven <eveha94@gmail.com>
2018-10-08 17:22:48 +0200

    sss

--- optimering-oblig1/test.py
+++ optimering-oblig1/test.py
@@ -11,7 +11,7 @@ def makeGraph(numberOfCities, graph):
                 distance = -1
                 graph[i][j] = distance
             else:
-                distance = random.randint(1, 100)
+                distance = random.randint(1, 10)
                 graph[i][j] = distance
                 graph[j][i] = distance
 
@@ -24,8 +24,16 @@ randTour = tsp.randomAlgorithm(graph)
 iterTour = tsp.iterativeRandom(graph, 10)
 greedyTour = tsp.greedyMethod(graph)
 
+""" randImpr = tsp.greedyImprovement(randTour, graph, 5000)
+iterImpr = tsp.greedyImprovement(iterTour, graph, 5000)
+greedImpr = tsp.greedyImprovement(greedyTour, graph, 5000) """
+
+randImpr = tsp.greedyRandomImprovement(randTour, graph, 100, 0.9)
 
 print('Cost rand:', tsp.calculateCost(randTour, graph))
-print('Cost iter:', tsp.calculateCost(iterTour, graph))
-print('Cost Greedy:', tsp.calculateCost(greedyTour, graph))
-
+print('Cost rand Impro:', tsp.calculateCost(randImpr, graph))
+""" print('Cost iter:', tsp.calculateCost(iterTour, graph))
+print('Cost iter impro:', tsp.calculateCost(iterImpr, graph))
+print('Cost greed:', tsp.calculateCost(greedyTour, graph))
+print('Cost greed impro:', tsp.calculateCost(greedImpr, graph))
+ """
--- optimering-oblig1/tsp.py
+++ optimering-oblig1/tsp.py
@@ -20,15 +20,13 @@ def randomAlgorithm(graph):
     startingNode = random.randint(0,len(graph) -1)
     visited[startingNode] = True
     tour.append(startingNode)
-    nodesVisited = 1
 
-    while (nodesVisited < len(graph)):
+    while (len(tour) < len(graph)):
         nextNode = random.randint(0,len(graph) -1)
         if (visited[nextNode]):
             continue
         visited[nextNode] = True
         tour.append(nextNode)
-        nodesVisited += 1
     
     return tour
 
@@ -40,12 +38,12 @@ def iterativeRandom(graph, numberOfTries):
     while (iterations < numberOfTries):
         currTour = randomAlgorithm(graph)
         if (iterations == 0):
-            bestTour = currTour
+            bestTour = numpy.array(currTour, copy=True)
             iterations += 1
             continue
 
         if (calculateCost(currTour, graph) < calculateCost(bestTour, graph)):
-            bestTour = currTour
+            bestTour = numpy.array(currTour, copy=True)
 
         iterations += 1
 
@@ -55,13 +53,12 @@ def greedyMethod(graph):
     tour = []
     visited = numpy.zeros((len(graph)), dtype='bool')
 
-    startingNode = random.randint(0,len(graph))
+    startingNode = random.randint(0,len(graph) - 1)
     visited[startingNode] = True
     tour.append(startingNode)
-    nodesVisited = 1
     prevNode = startingNode
 
-    while (nodesVisited < len(graph)):
+    while (len(tour) < len(graph)):
         lowestCost = sys.maxsize
         nearestNode = -1
 
@@ -78,6 +75,66 @@ def greedyMethod(graph):
         visited[nearestNode] = True
         prevNode = nearestNode
         tour.append(nearestNode)
-        nodesVisited +=1
     
     return tour
+
+def swapPlaces(tour, index1, index2):
+    node1 = tour[index1]
+    node2 = tour[index2]
+
+    tour[index1] = node2
+    tour[index2] = node1
+
+def greedyImprovement(initialTour, graph, numberOfTries):
+    iterations = 0
+    newTour = numpy.array(initialTour, copy=True)
+
+    while (iterations < numberOfTries):
+        index1 = random.randint(0, len(initialTour) - 1)
+        index2 = random.randint(0, len(initialTour) - 1)
+        currTour = numpy.array(newTour, copy=True)
+        
+        swapPlaces(currTour, index1, index2)
+        
+        oldCost = calculateCost(newTour, graph)
+        newCost = calculateCost(currTour, graph)
+
+        if (newCost < oldCost):
+            newTour = currTour
+        
+        iterations += 1
+
+    return newTour
+
+def greedyRandomImprovement(initialTour, graph, maxTries, poa):
+    bestTour = numpy.array(initialTour, copy=True)
+    bestCost = calculateCost(bestTour, graph)
+
+    while (poa > 0.000001):
+        oldTour = numpy.array(initialTour, copy=True)
+        oldCost = calculateCost(oldTour, graph)
+
+        for i in range(maxTries):
+            newTour = numpy.array(oldTour, copy=True)
+            index1 = random.randint(0, len(initialTour) - 1)
+            index2 = random.randint(0, len(initialTour) - 1)
+
+            swapPlaces(newTour, index1, index2)
+            newCost = calculateCost(newTour, graph)
+
+            if (newCost < oldCost):
+                oldCost = newCost
+                oldTour = numpy.array(newTour, copy=True)
+                if (newCost < bestCost):
+                    bestCost = newCost
+                    bestTour = numpy.array(newTour, copy=True)
+            else:
+                rnd = random.uniform(0,1)
+                
+                if (rnd < poa):
+                    oldCost = newCost
+                    oldTour = numpy.array(newTour, copy=True)
+            
+        poa = 0.9 * poa
+    
+    return bestTour
